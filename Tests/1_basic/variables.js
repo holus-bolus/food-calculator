15a4ec5795ec1a6f3723793b644c1f7859d57fed
Your Name <ustinovoleksij@gmail.com>
2022-09-25 14:25:35 +0300

    Added a calculator

--- Tests/1_basic/variables.js
+++
@@ -1,3 +0,0 @@
-const userName = "John";
-let userNumber = 25;
-userNumber = 24;
--- Tests/hard_tasks/hard_composition.js
+++
@@ -1,4 +0,0 @@
-// Максимально короткое решение :)
-const compose = (...fns) => (x) => fns.reduceRight((res, fn) => fn(res), x);
-
-const composeWithArgs = (...fns) => fns.reduceRight((f, g) => (...args) => g(f(...args)));
